ca82bd8b243d4e2062af0cd34fe01d537aad3912
SergeyForsh <seforsh@gmail.com>
2024-09-26 21:25:41 +0300

    Add files via upload

---
+++ module_1_3.ru.py
@@ -0,0 +1,14 @@
+name= 'Sergey'
+print(name, type (name))
+age = 57
+print(age, type (age))
+new_age = '58'
+print(new_age, type (new_age))
+name= 'is_student'
+print(name, type (name))
+name= 'Name: Sergey'
+print(name, type (name))
+name= 'age: 57'
+print(name, type (name))
+name= 'is_student: False'
+print(name, type (False))
