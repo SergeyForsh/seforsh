3e0e5759bef162b1bcde0f88a5e5ba39aed68475
SergeyForsh <seforsh@gmail.com>
2024-09-29 00:17:24 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,24 @@
+tuple_ = 1, 2, 3, 4, True, 'String' #кортеж неизменяемая
+print(tuple_)
+tuple_2 = (1, 2, 3, 4)
+print(tuple_2)
+tuple_3 = 1, 2, 3, 4
+tuple_ = 1, 2, 3, 4
+list_ = ([1, 2, 3, 4])
+print(type(tuple_3))
+print(type(tuple_[0]))
+print(tuple_[0])
+print(tuple_.__sizeof__())
+print(list_.__sizeof__())
+tuple_ = ([1, 2], 0)
+print(tuple_)
+tuple_ [0] [0]= 2
+print(tuple_)
+tuple_ = ([1, 2], 0) + (1, 2)
+print(tuple_)
+tuple_ = (1, 2) *3 #умножить (3 раза)
+print(tuple_)
+immutable_var_ = 1, 2, True, 'Str'
+print(immutable_var_)
+mutable_list_ =([1, 2, 3, 4])
+print(mutable_list_)
---
+++ my_string.py
@@ -0,0 +1,11 @@
+#name= input('Введите Ваше имя: ')
+#print('Здравствуйте,', name)
+#current_year = 2024
+#date_of_birth = int(input('В каком году вы родились?: '))
+#age = current_year - date_of_birth
+#print('В этом году Вам ', age, 'лет')
+print('привет, я строка в нижнем регистре'.upper().lower()) # .upper() верхний регистр .lower() нижний регистр
+print('привет, я строка в нижнем регистре'.replace('привет', 'пока'))
+print('привет, я строка в нижнем регистре'.replace(' ', '#'))
+print('my_string'.upper())
+print('my_string'.upper().lower())
