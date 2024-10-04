af00e508efcb37bf83720dcffdc61ac40b7addd7
SergeyForsh <seforsh@gmail.com>
2024-10-05 00:06:28 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict= {'Sergey': 1966, 'Elena': 1969, 'Dima': 1987, 'Evgeniy': 1988}
+print(my_dict)
+del my_dict ['Dima']
+print(my_dict)
+print(my_dict.values())
+my_set = {1,2,3,4,5,1,2,3,'String', True, (1,2,3)}
+print(my_set)
+print(my_set.add(10))
+print(my_set)
+
+
+
+
