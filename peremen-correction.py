e2c63374a3c84941f92571d9916969a9e3d8a138
SergeyForsh <seforsh@gmail.com>
2024-10-06 22:31:50 +0300

    Add files via upload

---
+++ peremen-correction.py
@@ -0,0 +1,29 @@
+name= 'Sergey'
+print(name)
+date_of_birth = 'September 1966'
+print(date_of_birth)
+dateOfBirth = 'September 1966'
+print(dateOfBirth)
+name= 'курс Python'
+print(name)
+name= 'количество заданий 12'
+print(name)
+name= 'затраченных часов 1,5'
+print(name)
+name= 'среднее время выполнения 0.125 часа'
+print(name)
+name= 'курс Python, '
+name_1 = 'количество заданий 12, '
+name_2 = 'затраченных часов 1,5, '
+name_3 = 'среднее время выполнения 0.125 часа'
+print(name, name_1, name_2, name_3)
+first_name = 'Vasya'
+apple_count = 10
+second_name = 'Petya'
+orange_count = 15
+print(first_name, 'дал', second_name,
+      apple_count, 'яблок и', orange_count, 'апельсинов')
+a= 5
+b= 6
+c= (5+6)
+print(a+b+c)
