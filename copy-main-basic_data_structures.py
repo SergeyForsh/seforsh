2acdfbe6921d6d3695ae4e23905107d6596872ff
SergeyForsh <seforsh@gmail.com>
2024-10-07 00:50:56 +0300

    Add files via upload

---
+++ copy-main-basic_data_structures.py
@@ -0,0 +1,33 @@
+print("Hello world")
+print("Hi Sergey")
+print(5)               # простые числа
+print(5.5)  # дробные числа
+print(type(5))   #тип целое число integer
+print(5 + 5)
+print(5*5)
+print(5/5)
+print(5//5)  # целочисленное деление
+print(5%5)   #0
+print(5**5)  #    возведение в степень
+print(type (5.5)) # плавающая точка, запятая  float
+print(type ('Hello Sergey'))    # string строка
+print('Hello "world"')              # строка      символы
+print('Hello,' + 'World!')   # сложение, склейка    # concatenate
+print(type (True), type (False ))  # boolean  логический тип данных
+print(5>10, 10>5, 1, 2, 'True', 'Hello Wopld')
+print(5>10 and 5==5)   #  and строго и лож...
+print(5<10 or 5==5)    #  or не строго  или верно.
+print(type(int('5')))
+print(5/5)
+print(5//5)
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98)
+print(9.99>9.98 and 1000!=1000.1)
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+#4th program
+print(123.456*10)
+print(int(float('123.456') * 10) / 10)
