aac003639c7dd6f9500c0440311acbac84379ecd
SergeyForsh <seforsh@gmail.com>
2024-10-09 21:50:39 +0300

    Add files via upload

---
+++ module_1_4_1.py
@@ -0,0 +1,28 @@
+name= input('Введите Ваше имя: ')          #str -строка! ввод внизу в терминале с клавиатуры
+print('Здравствуйте,', name)
+current_year = 2024                                  #int -число!
+date_of_birth = input('В каком году вы родились?: ') #str -строка! ввод внизу в терминале с клавиатуры
+age = current_year - int(date_of_birth)              #текущий год минус год рождения
+print('В этом году Вам ', age, 'лет')
+#my_string =input ('привет, я строка в верхнем регистре'.lower().upper())
+#print(my_string)
+#print('привет, я строка в нижнем регистре'.upper().lower())
+# .upper() верхний регистр .lower() нижний регистр
+print('my_string'.upper())
+print('my_string'.upper().lower())
+print('привет, я строка в нижнем регистре'.replace('привет', 'пока')) #замена слов
+print('привет, я строка в нижнем регистре'.replace(' ', '#'))         #убрать пробелы или #
+print()
+my_string = input('Введите текст: ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
+
+
