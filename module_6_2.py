4bd9ee3d900ff4084f52a05fc76bd85dc7ce23a5
SergeyForsh <seforsh@gmail.com>
2024-11-17 14:40:06 +0300

    Add files via upload

---
+++ module_6_2.py
@@ -0,0 +1,52 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner: str, model: str, color: str, engine_power: int):
+        self.owner = owner
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color if color.lower() in [c.lower() for c in self.__COLOR_VARIANTS] else 'unknown'
+
+    def get_model(self) -> str:
+        return f"Модель: {self.__model}"
+
+    def get_horsepower(self) -> str:
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    def get_color(self) -> str:
+        return f"Цвет: {self.__color}"
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f"Владелец: {self.owner}")
+
+    def set_color(self, new_color: str):
+        if new_color.lower() in [c.lower() for c in self.__COLOR_VARIANTS]:
+            self.__color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    def __init__(self, owner: str, model: str, color: str, engine_power: int):
+        super().__init__(owner, model, color, engine_power)
+
+
+# Пример использования классов
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+
+# Изначальные свойства
+vehicle1.print_info()
+
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')  # Попытка сменить цвет на недопустимый
+vehicle1.set_color('BLACK')  # Смена цвета на допустимый
+vehicle1.owner = 'Vasyok'  # Изменяем владельца
+
+# Проверяем что поменялось
+vehicle1.print_info()
